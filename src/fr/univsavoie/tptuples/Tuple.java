173f8c3d6a50e9c79a027cfa0b9a05474e6c7236
Silendel <greg.badin@gmail.com>
2014-02-28 09:40:26 +0100

    Added class pair and some code in other classes

--- src/fr/univsavoie/tptuples/Tuple.java
+++ src/fr/univsavoie/tptuples/Tuple.java
@@ -1,14 +1,56 @@
 package fr.univsavoie.tptuples;
 
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map.Entry;
 
 public class Tuple {
 	
-	private List<Entry<Object,TupleType>> Data;
+	private LinkedList<Entry<Object,TupleType>> data;
 	
-	public boolean match(TuplePattern pattern)	{
-		return false;
+	public int size() {
+		return data.size();
+	}
+
+	public Tuple (LinkedList<Entry<Object,TupleType>> data) {
+		this.data = data;
 	}
 	
+	public boolean match(TuplePattern pattern)	{
+		if(pattern.size() != this.size())
+			return false;
+		while()
+	}
+
+	public LinkedList<Entry<Object, TupleType>> getData() {
+		return data;
+	}
+
+	public void setData(LinkedList<Entry<Object, TupleType>> data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		String str = "Tuple [";
+		for(Entry<Object,TupleType> entry : data){
+			str += " "+entry.getKey();
+			switch(entry.getValue())
+			{
+			case STRING :
+				str +=", string;";
+				break;
+			case INTEGER :
+				str +=", int;";
+				break;
+			case FLOAT :
+				str +=", float;";
+				break;
+			default:
+				break;
+			}
+		}
+		str = str.substring(0, str.length()-2);
+		str += "]";
+		return str;
+	}
 }
--- src/fr/univsavoie/tptuples/TuplePattern.java
+++ src/fr/univsavoie/tptuples/TuplePattern.java
@@ -1,5 +1,26 @@
 package fr.univsavoie.tptuples;
 
-public class TuplePattern {
+import java.util.LinkedList;
+import java.util.Map.Entry;
 
+public class TuplePattern {
+	private LinkedList<Entry<Object,TupleType>> pattern;
+	
+	public int size() {
+		return pattern.size();
+	}
+
+	public TuplePattern(LinkedList<Entry<Object,TupleType>> pattern)
+	{
+		this.pattern = pattern;
+	}
+
+	public LinkedList<Entry<Object, TupleType>> getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(LinkedList<Entry<Object, TupleType>> pattern) {
+		this.pattern = pattern;
+	}
+	
 }
