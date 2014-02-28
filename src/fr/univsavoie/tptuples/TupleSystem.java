a23f6a51b119bc589f6de3fa131979f2b30fb89c
Silendel <greg.badin@gmail.com>
2014-02-28 08:54:22 +0100

    Added Two classes and define some skeletton function for the tupleSystem

--- src/fr/univsavoie/tptuples/TupleSystem.java
+++ src/fr/univsavoie/tptuples/TupleSystem.java
@@ -1,5 +1,16 @@
 package fr.univsavoie.tptuples;
 
 public class TupleSystem {
-
+	
+	public Tuple in (TuplePattern pattern) {
+		return new Tuple();
+	}
+	
+	public void out (Tuple tuple) {
+		
+	}
+	
+	public Tuple rd (TuplePattern pattern){
+		return new Tuple();
+	}
 }
