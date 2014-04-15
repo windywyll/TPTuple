184a11a791a338d7fd794ba0b9348d1b285f3435
jmaur <mauricejrd@gmail.com>
2014-04-15 14:49:36 +0200

    commit?

--- src/fr/univsavoie/tptuples/customer_get_counter_offer_transporter.java
+++ src/fr/univsavoie/tptuples/customer_get_counter_offer_transporter.java
@@ -3,7 +3,7 @@ package fr.univsavoie.tptuples;
 import java.util.LinkedList;
 import java.util.Random;
 
-public class customer_get_counter_offer_transporter extends Thread{
+public class Customer_get_counter_offer_transporter extends Thread{
 	public void run(){
 		TuplePattern patternIn;
 		Tuple readTuple;
@@ -42,18 +42,30 @@ public class customer_get_counter_offer_transporter extends Thread{
 			
 				//compute somethings;
 				LinkedList<TupleData> readData = readTuple.getData();
-				float cost = (float) readData.get(2).getData();
-				int time = (int) readData.get(3).getData();
+				float cost = (float) readData.get(1).getData();
+				int time = (int) readData.get(2).getData();
 				
 				//construction tupleOut
 				infosTuple = new LinkedList<TupleData>();
 				infosTuple.add(new TupleData("get_product_from_plant", TupleType.STRING));
 				infosTuple.add(new TupleData(cost, TupleType.FLOAT));
 				infosTuple.add(new TupleData(time, TupleType.INTEGER));
-				infosTuple.add(new TupleData( (int) readData.get(4).getData() , TupleType.INTEGER));
+				infosTuple.add(new TupleData( (int) readData.get(3).getData() , TupleType.INTEGER));
 				tupleOut = new Tuple(infosTuple);
 				
 				ts.out(tupleOut);
+				
+				System.out.println("transporter accepté");
+			}
+			else{
+				System.out.println("transporter refusé");
+			}
+			
+			try {
+				Thread.sleep(TupleSystem.SLEEP);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
